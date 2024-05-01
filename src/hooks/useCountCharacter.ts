1eed8d6d3b980c7228a2e1b395d899a547d869ab
shamo-9118 <won9118391183@gmail.com>
2024-05-02 05:52:17 +0900

    カスタムhookなのでuseをつける

---
+++ src/hooks/useCountCharacter.ts
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+export const useCountCharacter = (targetCharacter?: string): number => {
+  const [targetCharacterCount, setTargetCharacterCount] = useState<number>(0);
+
+  if (!targetCharacter) return targetCharacterCount;
+
+  const segmenter = new Intl.Segmenter();
+  const segmentedTargetCharacter = Array.from(segmenter.segment(targetCharacter));
+  setTargetCharacterCount([...segmentedTargetCharacter].length)
+
+  return targetCharacterCount;
+}
