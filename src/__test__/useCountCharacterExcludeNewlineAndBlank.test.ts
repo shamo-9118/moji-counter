3a39e7db0bc0d0bea9e1659ccff20204dbea4e3e
shamo-9118 <won9118391183@gmail.com>
2024-05-03 06:55:04 +0900

    テストケースをテストスイートにまとめる

--- src/__test__/useCountCharacterExcludeNewlineAndBlank.test.ts
+++ src/__test__/useCountCharacterExcludeNewlineAndBlank.test.ts
@@ -1,15 +1,17 @@
 import { renderHook } from '@testing-library/react';
 
-it('初期は0を表示', () => {
-  const { result } = renderHook(() => {
-    useCountCharacterExcludeNewlineAndBlank('');
+describe('空白と空行を除いて文字数をカウント', () => {
+  it('初期は0を表示', () => {
+    const { result } = renderHook(() => {
+      useCountCharacterExcludeNewlineAndBlank('');
+    });
+    expect(result.current.excludeNewLineCodeAndBlankCharacterCount).toBe(0);
   });
-  expect(result.current.excludeNewLineCodeAndBlankCharacterCount).toBe(0);
-});
 
-it('文字のみカウント', () => {
-  const { result } = renderHook(() => {
-    useCountCharacterExcludeNewlineAndBlank('丸 🥺\n');
+  it('文字のみカウント', () => {
+    const { result } = renderHook(() => {
+      useCountCharacterExcludeNewlineAndBlank('丸 🥺\n');
+    });
+    expect(result.current.excludeNewLineCodeAndBlankCharacterCount).toBe(2);
   });
-  expect(result.current.excludeNewLineCodeAndBlankCharacterCount).toBe(2);
 });
