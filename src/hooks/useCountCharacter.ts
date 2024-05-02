870e75fa1fe3d6c6a9f3f412c597b72baf122abc
shamo-9118 <won9118391183@gmail.com>
2024-05-02 21:14:33 +0900

    関数を外部かしたので呼び出しと型修正

--- src/hooks/useCountCharacter.ts
+++ src/hooks/useCountCharacter.ts
@@ -1,25 +1,10 @@
 import { useState } from 'react';
+import { countSegmentedTargetCharacter } from '../utils/countSegmentedTargetCharacter';
 
-import type { UseCountCharacter } from '../types/UseCountCharacter';
-
-export const useCountCharacter = (
-  targetCharacter?: string,
-): UseCountCharacter => {
+export const useCountCharacter = (targetCharacter: string): number => {
   const [targetCharacterCount, setTargetCharacterCount] = useState<number>(0);
 
-  const countSegmentedTargetCharacter = (
-    targetCharacter?: string,
-  ): number | undefined => {
-    if (!targetCharacter) return;
+  setTargetCharacterCount(countSegmentedTargetCharacter(targetCharacter));
 
-    // 改行コード・空白・マルチバイト文字を１文字としてカウント
-    const segmenter = new Intl.Segmenter();
-    const segmentedTargetCharacter = Array.from(
-      segmenter.segment(targetCharacter),
-    );
-
-    setTargetCharacterCount([...segmentedTargetCharacter].length);
-  };
-
-  return { targetCharacterCount, countSegmentedTargetCharacter };
+  return targetCharacterCount;
 };
