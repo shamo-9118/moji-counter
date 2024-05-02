90ac88152038fa398e11de875fa0f4dfcfeb63b9
shamo-9118 <won9118391183@gmail.com>
2024-05-02 23:40:14 +0900

    改行コードを除いて文字数をカウントするテスト作成

--- src/__test__/useCountCharacterExcludeNewlineCode.test.ts
+++ src/__test__/useCountCharacterExcludeNewlineCode.test.ts
@@ -6,3 +6,11 @@ it('改行コードを省いて文字数をカウントする処理の初期状�
 
   expect(result.current.excludeNewLineCodeCharacterCount).toBe(0);
 });
+
+it('改行コードを省いて文字数をカウントする処理', () => {
+  const { result } = renderHook(() =>
+    useCountCharacterExcludeNewlineCode('丸 🥺\n'),
+  );
+
+  expect(result.current.excludeNewLineCodeCharacterCount).toBe(3);
+});
