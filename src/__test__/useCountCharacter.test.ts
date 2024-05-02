0e58e8e99bf64430d4f7760f2aa78df4c06454ad
shamo-9118 <won9118391183@gmail.com>
2024-05-02 22:11:53 +0900

    customhooksの処理をテストする

---
+++ src/__test__/useCountCharacter.test.ts
@@ -0,0 +1,14 @@
+import { renderHook, act } from '@testing-library/react';
+import { useCountCharacter } from '../hooks/useCountCharacter';
+
+it('useCountCharacterの初期動作テスト', () => {
+  const { result } = renderHook(() => useCountCharacter(''));
+
+  expect(result.current.targetCharacterCount).toBe(0);
+});
+
+it('useCountCharacterの文字数検証時のテスト', () => {
+  const { result } = renderHook(() => useCountCharacter('丸 🥺\n'));
+
+  expect(result.current.targetCharacterCount).toBe(4);
+});
