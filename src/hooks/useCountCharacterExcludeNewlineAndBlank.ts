60339734d4d735e52fdf244b45e9d20b938d7682
shamo-9118 <won9118391183@gmail.com>
2024-05-03 10:12:51 +0900

    改行コードと空白を省く処理を作成

---
+++ src/hooks/useCountCharacterExcludeNewlineAndBlank.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { dividSegmentTargetCharacter } from '../utils/dividSegmentTargetCharacter';
+
+export const useCountCharacterExcludeNewlineAndBlank = (
+  targetCharacter: string,
+): { excludeNewLineAndBlankCharacterCount: number } => {
+  const [
+    excludeNewLineAndBlankCharacterCount,
+    setExcludeNewLineAndBlankCharacterCount,
+  ] = useState<number>(0);
+
+  useEffect(() => {
+    const segmentedTextWithNewLineAndWhitespaceRemoved =
+      dividSegmentTargetCharacter(targetCharacter).filter((segmentedText) => {
+        return (
+          segmentedText.segment !== '\n' &&
+          segmentedText.segment !== ' ' &&
+          segmentedText.segment !== '\u3000'
+        );
+      });
+    setExcludeNewLineAndBlankCharacterCount(
+      segmentedTextWithNewLineAndWhitespaceRemoved.length,
+    );
+  }, [targetCharacter]);
+
+  return { excludeNewLineAndBlankCharacterCount };
+};
