f705a1f2e64535faf3afc6f614ebb796be0996ba
shamo-9118 <won9118391183@gmail.com>
2024-05-04 22:28:24 +0900

    テストに使用する関数名を修正

--- src/__test__/useCountLine.test.ts
+++ src/__test__/useCountLine.test.ts
@@ -5,7 +5,7 @@ describe('改行コードを省いて文字数を数える処理のテスト', (
   it('初期は0を表示', () => {
     const { result } = renderHook(() => useCountLine(''));
 
-    expect(result.current.excludeNewLineCharacterCount).toBe(0);
+    expect(result.current.targetLineCount).toBe(0);
   });
 
   it('文字のある行数をカウント', () => {
@@ -13,7 +13,7 @@ describe('改行コードを省いて文字数を数える処理のテスト', (
       useCountLine('丸本はよく\n丸木と\n間違えられます🥺'),
     );
 
-    expect(result.current.excludeNewLineCharacterCount).toBe(3);
+    expect(result.current.targetLineCount).toBe(3);
   });
 
   it('文字のない行はカウントしない', () => {
@@ -21,6 +21,6 @@ describe('改行コードを省いて文字数を数える処理のテスト', (
       useCountLine('丸本はよく\n丸木と\n間違えられます🥺\n'),
     );
 
-    expect(result.current.excludeNewLineCharacterCount).toBe(3);
+    expect(result.current.targetLineCount).toBe(3);
   });
 });
