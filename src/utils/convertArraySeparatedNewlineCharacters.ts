a43d2b6af0f5d905febf35213f8b783b0848ea29
shamo-9118 <won9118391183@gmail.com>
2024-05-08 06:17:48 +0900

    配列の末尾に空文字が入る場合を考慮して処理修正

--- src/utils/convertArraySeparatedNewlineCharacters.ts
+++ src/utils/convertArraySeparatedNewlineCharacters.ts
@@ -1,9 +1,18 @@
+import { removeEmptyInEndOfArray } from './';
+
 export const convertArraySeparatedNewlineCharacters = (
   targetCharacter: string,
 ): string[] => {
   // 改行コード・空白・マルチバイト文字を１文字としてカウント
-  const splitedCharaceterListByNewLine = targetCharacter
-    .split('\n')
-    .filter((splitedCharaceter) => splitedCharaceter !== '');
-  return splitedCharaceterListByNewLine;
+  const splitedCharaceterListByNewLine = targetCharacter.split('\n');
+  const isEmptyAtEndOfArray =
+    splitedCharaceterListByNewLine[
+      splitedCharaceterListByNewLine.length - 1
+    ] === '';
+
+  if (!isEmptyAtEndOfArray) {
+    return splitedCharaceterListByNewLine;
+  }
+
+  return removeEmptyInEndOfArray(splitedCharaceterListByNewLine);
 };
