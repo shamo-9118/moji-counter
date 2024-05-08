4dcbc3aff59fabdf5b4f8f9f7d3964e481060efd
shamo-9118 <won9118391183@gmail.com>
2024-05-08 22:41:19 +0900

    Footerコンポーネント作成

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+export const Footer = () => {
+  return (
+    <footer
+      style={{
+        textAlign: 'center',
+        padding: '20px',
+        backgroundColor: '#333',
+        color: '#fff',
+      }}
+    >
+      This is moji_counter.
+    </footer>
+  );
+};
