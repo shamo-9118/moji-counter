1d432d7fc0963e4ab21a509b27871db2931e5b0f
shamo-9118 <won9118391183@gmail.com>
2024-05-09 00:01:49 +0900

    パスを修正

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,4 @@
-export * from '../hooks/useCountCharacter';
-export * from '../hooks/useCountCharacterExcludeNewLine';
+export * from './useCountCharacter';
+export * from './useCountCharacterExcludeNewLine';
 export * from './useCountCharacterExcludeNewLineAndBlank';
-export * from '../hooks/useCountLine';
+export * from './useCountLine';
