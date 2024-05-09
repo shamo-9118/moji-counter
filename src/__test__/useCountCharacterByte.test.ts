3e52bc45070992c2d3c77a51a878900299e84b7a
shamo-9118 <won9118391183@gmail.com>
2024-05-10 06:04:10 +0900

    字列のバイト数を計算する処理のテスト作成字列のバイト数を計算する処理のテスト作成

--- src/__test__/useCountCharacterByte.test.ts
+++ src/__test__/useCountCharacterByte.test.ts
@@ -1,8 +1,42 @@
 import { renderHook } from '@testing-library/react';
 import { useCountCharacterByte } from '../hooks';
 
-test('', () => {
-  const { result } = renderHook(() => useCountCharacterByte(''));
+describe('文字数のバイトを計算する処理のテスト', () => {
+  test('初期表示', () => {
+    const { result } = renderHook(() => useCountCharacterByte(''));
 
-  expect(result.current.encodedCharBytes).toBe(0);
+    expect(result.current.encodedCharBytes).toBe(0);
+  });
+
+  test('半角英字', () => {
+    const { result } = renderHook(() => useCountCharacterByte('ee'));
+
+    expect(result.current.encodedCharBytes).toBe(2);
+  });
+
+  test('全角英字', () => {
+    const { result } = renderHook(() => useCountCharacterByte('ｌｌｌ'));
+
+    expect(result.current.encodedCharBytes).toBe(9);
+  });
+
+  test('日本語', () => {
+    const { result } = renderHook(() => useCountCharacterByte('丸本将真'));
+
+    expect(result.current.encodedCharBytes).toBe(12);
+  });
+
+  test('特殊文字', () => {
+    const { result } = renderHook(() => useCountCharacterByte('🤩🏠👪'));
+
+    expect(result.current.encodedCharBytes).toBe(12);
+  });
+
+  test('いろんな文字混合', () => {
+    const { result } = renderHook(() =>
+      useCountCharacterByte('🤩まる丸maru M\n'),
+    );
+
+    expect(result.current.encodedCharBytes).toBe(20);
+  });
 });
