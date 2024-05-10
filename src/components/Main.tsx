1063e851b454edce1719ecb3b65d66429693b621
shamo-9118 <won9118391183@gmail.com>
2024-05-10 22:54:45 +0900

    数字の表記を右揃えに

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -53,23 +53,29 @@ export const Main = () => {
           <Table.Tbody>
             <Table.Tr>
               <Table.Th>文字数</Table.Th>
-              <Table.Td>{countCharacter}</Table.Td>
+              <Table.Td style={{ textAlign: 'end' }}>{countCharacter}</Table.Td>
             </Table.Tr>
             <Table.Tr>
               <Table.Th>文字数（改行なし）</Table.Th>
-              <Table.Td>{countCharacterExcludeNewLine}</Table.Td>
+              <Table.Td style={{ textAlign: 'end' }}>
+                {countCharacterExcludeNewLine}
+              </Table.Td>
             </Table.Tr>
             <Table.Tr>
               <Table.Th>文字数（改行なし・空白なし）</Table.Th>
-              <Table.Td>{countCharacterExcludeNewLineAndBlank}</Table.Td>
+              <Table.Td style={{ textAlign: 'end' }}>
+                {countCharacterExcludeNewLineAndBlank}
+              </Table.Td>
             </Table.Tr>
             <Table.Tr>
               <Table.Th>バイト数（UTF-8）</Table.Th>
-              <Table.Td>{countCharacterByte}</Table.Td>
+              <Table.Td style={{ textAlign: 'end' }}>
+                {countCharacterByte}
+              </Table.Td>
             </Table.Tr>
             <Table.Tr>
               <Table.Th>行数</Table.Th>
-              <Table.Td>{countLine}</Table.Td>
+              <Table.Td style={{ textAlign: 'end' }}>{countLine}</Table.Td>
             </Table.Tr>
           </Table.Tbody>
         </Table>
